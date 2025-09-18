9c6520c46f6cfd9ac574b6b956531600c66aaf98
HimanshuP601 <himanshuparate601@gmail.com>
2025-09-18 13:52:02 +0530

    Added workflow for automatic activity updates

--- content.js
+++
@@ -1,7 +0,0 @@
-// Detect page visit and send to background
-chrome.runtime.sendMessage({
-    type: "PAGE_VISIT",
-    site: window.location.hostname,
-    url: window.location.href,
-    title: document.title
-});
